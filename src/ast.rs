187430a9b20270c57db9791395562c2f6fecbefd
shreyasm-dev <62489066+shreyasm-dev@users.noreply.github.com>
2023-08-15 01:10:11 +0000

    Create ast

---
+++ src/ast.rs
@@ -0,0 +1,86 @@
+use std::path::Path;
+
+pub struct Program {
+  pub statements: Vec<Statement>,
+}
+
+pub enum Statement {
+  Block {
+    statements: Vec<Statement>,
+  },
+  LetStatement {
+    name: Identifier,
+    value: Expression,
+    constant: bool,
+  },
+  ExpressionStatement {
+    expression: Expression,
+  },
+  FunctionDeclaration {
+    name: Identifier,
+    parameters: Vec<Identifier>,
+    body: Box<Statement>,
+  },
+}
+
+pub enum Expression {
+  Void,
+  Identifier(Identifier),
+  NumberLiteral(f64),
+  StringLiteral(&'static str),
+  BooleanLiteral(bool),
+  PathLiteral(&'static Path),
+  ArrayLiteral(Vec<Expression>),
+  ShellBlock {
+    shell: Option<&'static Path>,
+    content: &'static str,
+  },
+  FunctionCall {
+    parameters: Vec<Identifier>,
+    body: Box<Statement>,
+  },
+  If {
+    condition: Box<Expression>,
+    consequence: Box<Statement>,
+    alternative: Option<Box<Statement>>,
+  },
+  For {
+    variable: Identifier,
+    iterable: Box<Expression>,
+    body: Box<Statement>,
+  },
+  Binary {
+    operator: BinaryOperator,
+    left: Box<Expression>,
+    right: Box<Expression>,
+  },
+  Unary {
+    operator: UnaryOperator,
+    operand: Box<Expression>,
+  },
+}
+
+pub struct Identifier {
+  pub value: &'static str,
+}
+
+pub enum BinaryOperator {
+  Add,
+  Subtract,
+  Multiply,
+  Divide,
+  Modulo,
+  Equal,
+  NotEqual,
+  LessThan,
+  LessThanOrEqual,
+  GreaterThan,
+  GreaterThanOrEqual,
+  And,
+  Or,
+}
+
+pub enum UnaryOperator {
+  Negate,
+  Not,
+}
